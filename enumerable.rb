c26a7b2a63a6e3fbf5e1cd1fe7923be4c3e730d5
dev <dev@localhost.localdomain>
2020-10-01 19:05:29 +0300

    Added my all my_inject enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -117,7 +117,19 @@ module Enumerable
         new_array
     end
 
-    
-
+    def my_inject(*args)
+        x = args[0] if args[0].is_a?(Integer)
+        operator = args[0].is_a?(Symbol) ? args[0] : args[1]
+        if operator
+          each do |n|
+            x = x ? x.send(operator, n) : n
+          end
+          return x
+        end
+        each do |n|
+          x = x ? yield(x, n) : n
+        end
+        x
+    end
 end
 # rubocop:enable Metrics/ModuleLength, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity, Style/CaseEquality
